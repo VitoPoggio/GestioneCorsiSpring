e3990a9d289c64a54c46d4dadec623d7399a12ee
VitoDebellis <vito.debellis.1993@gmail.com>
2022-12-28 10:53:17 +0100

    Commit progetto

---
+++ GestioneCorsiSpring/src/main/java/com/betacom/corsi/GestioneCorsiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.corsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneCorsiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneCorsiSpringApplication.class, args);
+	}
+
+}
---
+++ GestioneCorsiSpring/src/test/java/com/betacom/corsi/GestioneCorsiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.corsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneCorsiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
